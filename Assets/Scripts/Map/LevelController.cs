5934edc1c6ff9c104deab64f901e3acf68298b44
Estiem <sergi.t.m@hotmail.com>
2024-12-05 22:08:38 +0100

    add final boss music

--- Assets/Scripts/Map/LevelController.cs
+++ Assets/Scripts/Map/LevelController.cs
@@ -8,23 +8,53 @@ public class LevelController : MonoBehaviour
 {
     private int currentFloor;
     public int maxFloors;
+    public AudioSource audioSource;
+    public AudioClip lobbyMusic;
+    public AudioClip dungeonMusic;
+    public AudioClip finalBossMusic;
+    
 
     void Start(){
+        audioSource = GetComponent<AudioSource>();
+
         DontDestroyOnLoad(this.gameObject);
-        currentFloor = 1;
+        currentFloor = 0;
     }
 
     public void onFloorFinished(){
+        currentFloor++;
+        Debug.LogFormat("STM - currentFloor = {0}", currentFloor);
+        
         if(currentFloor < maxFloors){
             SceneManager.LoadScene("Level1");
-            currentFloor++;
-        } else {
-            SceneManager.LoadScene("LobbyScene");
+
+            audioSource.Stop();
+            if(dungeonMusic != null){
+                audioSource.clip = dungeonMusic;
+                audioSource.Play();   
+            }
+
+        } else if(currentFloor == maxFloors){
+            SceneManager.LoadScene("FinalBoss");
+
+            audioSource.Stop();
+            if(finalBossMusic != null){
+                audioSource.clip = finalBossMusic;
+                audioSource.Play();   
+            }
+        } else if(currentFloor > maxFloors){
             resetFloors();
         }
     }
 
     public void resetFloors(){
-        currentFloor = 1;
+        currentFloor = 0;
+        SceneManager.LoadScene("LobbyScene");
+
+        audioSource.Stop();
+        if(lobbyMusic != null){
+            audioSource.clip = lobbyMusic;
+            audioSource.Play();   
+        }
     }
 }
